783991b6933a3fcff6817d53d5552672d4b4aa4e
vieck <vieck@purdue.edu>
2016-11-30 17:35:00 -0500

    Re-added login

--- app/login/login.js
+++ app/login/login.js
@@ -15,5 +15,25 @@ angular.module('myApp.login', ['ngRoute'])
     .controller('LoginCtrl', function ($scope, $location) {
 
         $scope.base_url = "https://www.google.com";
+        $scope.login = {
+            user: '',
+            password: ''
+        };
 
+        $scope.authenticate = function () {
+            // POST request for logging in a user
+            $http({
+                method: 'POST',
+                url: $scope.base_url + '/some-endpoint',
+                withCredentials: true,
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                data: $.param({name: $scope.login.user, password: $scope.login.password})
+            }).then(function successCallback(response) {
+                //All data returned will be under response.data
+            }, function errorCallback(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+
+            });
+        }
     });
