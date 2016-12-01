e4ec8fc605904073db2fa1d0c1e0ac8ed07181f5
Joseph Khawly <josephkhawly@gmail.com>
2016-11-30 19:29:07 -0500

    started registration portion of backend.

---
+++ app/register/Register.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ComparisonOperator;
+import com.amazonaws.services.dynamodbv2.model.Condition;
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
+import com.amazonaws.services.dynamodbv2.model.DescribeTableRequest;
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
+import com.amazonaws.services.dynamodbv2.model.KeyType;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
+import com.amazonaws.services.dynamodbv2.model.PutItemResult;
+import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
+import com.amazonaws.services.dynamodbv2.model.TableDescription;
+import com.amazonaws.services.dynamodbv2.util.TableUtils;
+
+public class Register {
+
+  private static Map<String, AttributeValue> addUser(String username, String password, String email, String hash) {
+      Map<String, AttributeValue> user = new HashMap<String, AttributeValue>();
+      user.put("Username", new AttributeValue(username));
+      user.put("year", new AttributeValue(password));
+      user.put("rating", new AttributeValue(email));
+      user.put("fans", new AttributeValue(hash));
+
+      return user;
+  }
+
+  public static void main(String[] args) throws Exception {
+
+  }
+}
