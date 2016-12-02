5cf7acd6601b7c4a16d45f338105d784384a5ff5
Lisa Campbell <campb215@purdue.edu>
2016-12-02 12:02:26 -0500

    linked workflow

--- app/register/register.js
+++ app/register/register.js
@@ -22,25 +22,22 @@ angular.module('myApp.register', ['ngRoute'])
 
         $scope.base_url = "https://planit.mybluemix.net";
 
-        $scope.create = function () {
+        $scope.create = function ($routeProvider) {
             // POST request for creating a user
-            $http({
-                method: 'POST',
-                url: $scope.base_url + '/register',
-                withCredentials: true,
-                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-                data: {
-                    first: $scope.register.first,
-                    last: $scope.register.last,
-                    name: $scope.register.user,
-                    password: $scope.register.password
-                }
-            }).then(function successCallback(response) {
-                //All data returned will be under response.data
-            }, function errorCallback(response) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-
-            });
+            // $http({
+            //     method: 'POST',
+            //     url: $scope.base_url + '/register',
+            //     withCredentials: true,
+            //     headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            //     data: {
+            //         first: $scope.register.first,
+            //         last: $scope.register.last,
+            //         name: $scope.register.user,
+            //         password: $scope.register.password
+            //     }
+            // }).then(function successCallback(response) {
+            //     //All data returned will be under response.data
+            // });
+            // $routeProvider.otherwise({redirectTo: '/projects'});
         }
     });
