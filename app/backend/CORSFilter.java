b8d1126d3f1a0359123112f504bc844cf8c3100a
vieck <vieck@purdue.edu>
2016-12-05 10:00:03 -0500

    I had a bug in cors filter

--- app/backend/CORSFilter.java
+++ app/backend/CORSFilter.java
@@ -15,7 +15,7 @@ public class CORSFilter implements Filter {
         System.out.println("Filtering on...........................................................");
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:8000");
-        header("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "X-requested-with, Content-Type");
