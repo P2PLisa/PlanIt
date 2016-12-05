0ea6f8616d870da9e084903e114acc743fbc4b38
vieck <vieck@purdue.edu>
2016-12-05 10:55:51 -0500

    Added themes

--- app/app.js
+++ app/app.js
@@ -39,7 +39,19 @@ angular.module('myApp', [
             }
         };
     }
-]).config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+]).config(['$locationProvider', '$routeProvider', '$mdThemingProvider', function ($locationProvider, $routeProvider, $mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+        .primaryPalette('deep-purple', {
+            'default': '900', // by default use shade 400 from the pink palette for primary intentions
+            'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+            'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+            'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+        })
+        // If you specify less than all of the keys, it will inherit from the
+        // default shades
+        .accentPalette('cyan', {
+            'default': 'A100' // use shade 200 for default, and keep all other shades the same
+        });
     $locationProvider.hashPrefix('!');
 
     $routeProvider.otherwise({redirectTo: '/home'});
