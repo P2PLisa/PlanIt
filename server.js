98b05439c3dae81d311725bc94c3b5f389fa7b23
Lisa Campbell <campb215@purdue.edu>
2016-12-05 13:56:59 -0500

    because reasons

--- server.js
+++ server.js
@@ -128,9 +128,9 @@ app.post('/register/:username', function(request, response, next){
 	console.log("Adding a new item...");
 	docClient.put(params, function(err, data) {
 		if (err) {
-		console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+		console.error("Unable to add item. Error JSON:", JSON.stringify(err, " ", 2));
 		} else {
-		console.log("Added item:", JSON.stringify(data, null, 2));
+		console.log("Added item:", JSON.stringify(data, " ", 2));
 		}
 		});
  //    var params = {
