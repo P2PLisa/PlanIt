b3b864419d9b35981b7b4cf7dc08017b47cd96bb
Lisa Campbell <campb215@purdue.edu>
2016-12-05 01:00:50 -0500

    grr auth stuff

--- server.js
+++ server.js
@@ -8,28 +8,32 @@ var port = process.env.PORT || 8080;
 // make express look in the right directory...
 app.use(express.static(__dirname));
 
-app.use(cors({origin: 'http://www.planit.mybluemix.net'}));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // set the home page route
-app.get('/', function(req, res) {
+app.get('/', function(req, res, next) {
 
     // make sure index is in the right directory. In this case /index.html
     res.render('index');
 });
 
-app.get('/register/:username/', function(request, response){
+app.get('/register/:username/', function(request, response, next){
   // Now we automatically get the story in the request object
   // We use story ID to create a new element for that story
   response.send({ username: request.username, password: "newElement"});
 }); 
 
-app.get('/signin/:username/', function(request, response){
+app.get('/signin/:username/', function(request, response, next){
   // Now we automatically get the story in the request object
   // We use story ID to create a new element for that story
   response.send({ username: request.username, password: "newElement"});
 }); 
 
-app.post('/signin/:username/', function(request, response){
+app.post('/signin/:username/', function(request, response, next){
   response.send({ username: request.username, password: "newElement"});
 }); //post
 
